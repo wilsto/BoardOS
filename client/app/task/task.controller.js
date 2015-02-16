823a6934c1f37deea45c32603fbc2c42f14baa60
wilsto <willy.stophe.pro@gmail.com>
2015-02-17 00:50:50 +0100

    Release v0.11.5

--- client/app/task/task.controller.js
+++ client/app/task/task.controller.js
@@ -116,4 +116,51 @@ angular.module('boardOsApp')
             });
         };
 
+
+        $scope.showWeeks = true;
+
+        $scope.today = function() {
+            $scope.date = new Date();
+        };
+        $scope.today();
+
+        $scope.toggleWeeks = function() {
+            $scope.showWeeks = !$scope.showWeeks;
+        };
+
+        $scope.clear = function() {
+            $scope.date = null;
+        };
+
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+            return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+        };
+
+        $scope.toggleMin = function() {
+            $scope.minDate = ($scope.minDate) ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.open1 = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened1 = true;
+        };
+
+        $scope.open2 = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened2 = true;
+        };
+
+        $scope.dateOptions = {
+            'year-format': '"yyyy"',
+            'starting-day': 1
+        };
+
+        $scope.format = 'dd-MMMM-yyyy';
+
     });
