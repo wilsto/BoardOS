9295db885ef4b5090625b97673849106a89b107c
wilsto <willy.stophe.pro@gmail.com>
2017-05-01 18:28:27 +0200

    Release v1.6.32

--- Gruntfile.js
+++ Gruntfile.js
@@ -437,7 +437,7 @@ module.exports = function(grunt) {
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
         push: true,
-        pushTo: 'origin master',
+        pushTo: 'origin',
         gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
         globalReplace: false
       }
