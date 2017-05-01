ff503acb51dadc256e0f6008269ff8be25c69ad1
wilsto <willy.stophe.pro@gmail.com>
2017-05-01 18:06:11 +0200

    Release v1.6.30

--- server/api/thing/thing.controller.js
+++ server/api/thing/thing.controller.js
@@ -14,17 +14,28 @@ var Thing = require('./thing.model');
 
 // Get list of things
 exports.index = function(req, res) {
-  Thing.find(function (err, things) {
-    if(err) { return handleError(res, err); }
+  Thing.find(function(err, things) {
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(200, things);
   });
 };
 
 // Get a single thing
 exports.show = function(req, res) {
-  Thing.findById(req.params.id, function (err, thing) {
-    if(err) { return handleError(res, err); }
-    if(!thing) { return res.send(404); }
+  return res.json('thing');
+};
+
+// Get a single thing
+exports.show2 = function(req, res) {
+  Thing.findById(req.params.id, function(err, thing) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!thing) {
+      return res.send(404);
+    }
     return res.json(thing);
   });
 };
@@ -32,20 +43,30 @@ exports.show = function(req, res) {
 // Creates a new thing in the DB.
 exports.create = function(req, res) {
   Thing.create(req.body, function(err, thing) {
-    if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(201, thing);
   });
 };
 
 // Updates an existing thing in the DB.
 exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  Thing.findById(req.params.id, function (err, thing) {
-    if (err) { return handleError(res, err); }
-    if(!thing) { return res.send(404); }
+  if (req.body._id) {
+    delete req.body._id;
+  }
+  Thing.findById(req.params.id, function(err, thing) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!thing) {
+      return res.send(404);
+    }
     var updated = _.merge(thing, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
+    updated.save(function(err) {
+      if (err) {
+        return handleError(res, err);
+      }
       return res.json(200, thing);
     });
   });
@@ -53,11 +74,17 @@ exports.update = function(req, res) {
 
 // Deletes a thing from the DB.
 exports.destroy = function(req, res) {
-  Thing.findById(req.params.id, function (err, thing) {
-    if(err) { return handleError(res, err); }
-    if(!thing) { return res.send(404); }
+  Thing.findById(req.params.id, function(err, thing) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!thing) {
+      return res.send(404);
+    }
     thing.remove(function(err) {
-      if(err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.send(204);
     });
   });
